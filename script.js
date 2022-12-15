7a90ce878e2d58d53518f874c6e7e74c96435814
abuki43 <120245789+abuki43@users.noreply.github.com>
2022-12-15 22:54:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+ let mainContainer = document.querySelector("#rating-page")
+ let thanksContainer = document.querySelector(".thanks-page")
+ let submitButton = document.querySelector(".submit-btn")
+ let ratings = document.querySelectorAll(".btn-num")
+ let actualRating = document.querySelector("#rating")
+
+function submit() {
+   mainContainer.style.display = "none"
+   thanksContainer.classList.remove("hide")
+   }
+
+//   ratings.forEach((rate) => {
+//     rate.addEventListener("click" , () => {
+//       actualRating.innerHTML = rate.innerHTML
+//     })
+//   })
+// }
+
+
+
+
+
+
+
+
+  
